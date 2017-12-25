84ad10bbb06009e58796836d49e0cb0d07a0abf8
FryOS <alexeyosp@gmail.com>
2017-12-25 18:04:04 +0300

    while (target != elements[i])  Добавил i элемент в сравнение

--- my_app/script.js
+++ my_app/script.js
@@ -1,30 +1,34 @@
-var selectedCell;
+let selectedCell;
 
-var elements = document.querySelectorAll("div.table");
+var elements = document.querySelectorAll("div.table .cell");
 for (var i = 0; i < elements.length; i++) {
   elements[i].onclick = function(event){
     
         var target = event.target;
         console.log(target);
         // цикл двигается вверх от target к родителям до table
-        while (target != elements) {
+        while (target != elements[i]) {
           if (target.classList == 'cell') {
+              console.log(target.classList);
             // нашли элемент, который нас интересует!
             highlight(target);
             return;
           }
           target = target.parentNode;
         }
-     
   };
   
 } 
 
 function highlight(node) {
-  if (selectedCell) {
-    selectedCell.classList.remove('active');
-  }
+    if (selectedCell) {
+        selectedCell.classList.remove('active');
+        
+       
+      } 
+
   selectedCell = node;
+  console.log(selectedCell);
   selectedCell.classList.add('active');
 }
 
